046b2bf5d76c5e5dc9294da40896fbdb0ffe1698
21KLA5 <111718383+21KLA5@users.noreply.github.com>
2024-06-29 15:47:14 -0400

    Tells User to navigate to onQ grades section

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,9 @@
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs){
   var url = String(tabs[0].url)
-  if (url.includes("https://onq.queensu.ca/d2l/lms/grades/my_grades")){
-    chrome.runtime.sendMessage({action: "getGrades()"}
-    )
+  var header = document.getElementById("text");
+  if (url.includes("https://onq.queensu.ca/d2l/lms/grades/my_grades")){ 
+    header.textContent = "calculating grade in course..."
+  }else{
+    header.textContent = "navigate to onQ grades tab!"; 
   }
 })
--- scripts/content.js
+++
@@ -1,3 +0,0 @@
-function getGrades() {
-    console.log("It works")
-  }
