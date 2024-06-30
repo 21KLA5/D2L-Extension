3b110fd0aba13f4fcbfa818bbe427bd8d24b6bc9
21KLA5 <111718383+21KLA5@users.noreply.github.com>
2024-06-30 17:45:31 -0400

    Accessing Table

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,11 @@
 const grid = document.getElementsByClassName("d2l-table d2l-grid d_gl");
-console.log(grid);
+const myTable = grid[0];
+const numRows = myTable.rows.length;
+const weightIndex = 2; 
+const headerRow = myTable.rows[1];
+
+console.log(headerRow.cells[weightIndex].innerText); 
 
 // Store the data in chrome.storage.local
 chrome.storage.local.set({ number: grid }, () => {
-  console.log("Grade stored");
 });
