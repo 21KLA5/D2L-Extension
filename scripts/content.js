e91b04dd28b4684cb62ca9e04bf5db8ef6865faf
21KLA5 <111718383+21KLA5@users.noreply.github.com>
2024-07-01 18:10:57 -0400

    Added Calculate Current Grade Functionality

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,37 @@
 const grid = document.getElementsByClassName("d2l-table d2l-grid d_gl");
 const myTable = grid[0];
 const numRows = myTable.rows.length;
-const weightIndex = 2; 
-const headerRow = myTable.rows[1];
+var weightIndex; 
+var weightsList = []; 
 
-console.log(headerRow.cells[weightIndex].innerText); 
+var tempWeight;
+var tempFloatWeight;
+var sumWeight = 0; 
 
-// Store the data in chrome.storage.local
-chrome.storage.local.set({ number: grid }, () => {
+
+/*var rows = myTable.getElementsByTagName('tr');
+rows = Array.from(rows);
+rows.forEach(function(row) {
+  if (row.classList.contains('d_ggl1')) {
+    row.parentNode.removeChild(row);
+  }
+});
+*/
+
+for (i = 1; i < numRows; i++){
+    weightIndex = 2; 
+    if (myTable.rows[i].cells[0].innerText.trim() === ''){
+        weightIndex = 3; 
+    }
+    if (!myTable.rows[i].classList.contains('d_ggl1')){
+        weightsList[i] = myTable.rows[i].cells[weightIndex].innerText; 
+        tempWeight = weightsList[i].split(" / ")[0];
+        tempFloatWeight = parseFloat(tempWeight);
+        sumWeight = sumWeight + tempFloatWeight; 
+    }
+}
+
+sumWeight = sumWeight.toFixed(2);
+
+chrome.storage.local.set({ number: sumWeight }, () => {
 });
