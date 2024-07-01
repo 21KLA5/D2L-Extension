daa68ee5b1ba859d29a4b0b8fb4d54f68a245dc0
21KLA5 <111718383+21KLA5@users.noreply.github.com>
2024-07-01 19:11:40 -0400

    Added UI and GPA function

--- popup/popup.js
+++ popup/popup.js
@@ -2,25 +2,40 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs){
   var url = String(tabs[0].url)
   var header = document.getElementById("text");
   if (url.includes("https://onq.queensu.ca/d2l/lms/grades/my_grades")){ 
-    header.textContent = "Click below to find out your grade in this class: "
+    header.textContent = "What would you like to do? "
   }else{
-    header.textContent = "Please navigate to onQ grades tab!"; 
-    document.getElementById("myButton").style.display = 'none';
+    header.textContent = "No Data Found!\n You may be on the wrong page.\n Please navigate to onQ grades tab!"; 
+    document.getElementById("myButton1").style.display = 'none';
+    document.getElementById("myButton2").style.display = 'none';
+    document.getElementById("myButton3").style.display = 'none';
   }
 
 
-  document.getElementById('myButton').addEventListener('click', function() {
-    console.log('test');
-    // Retrieve the stored data when the popup is loaded
-    chrome.storage.local.get(['number'], function(result) {
-      if (result.number) {
-        document.getElementById("grades").textContent = result.number;
+  document.getElementById('myButton1').addEventListener('click', function() {
+    chrome.storage.local.get(['number1'], function(result) {
+      if (result.number1) {
+        var button1 = document.getElementById("myButton1");
+        button1.textContent = result.number1 + '%';
+        button1.id = "normalButton"; 
+
       } else {
-        document.getElementById("grades").textContent = "No data found";
+        document.getElementById("myButton1").textContent = "No data found";
       }
     });
   })
 
+  document.getElementById('myButton3').addEventListener('click', function() {
+    chrome.storage.local.get(['number3'], function(result) {
+      if (result.number3) {
+        var button1 = document.getElementById("myButton3");
+        button1.textContent = result.number3;
+        button1.id = "normalButton"; 
+
+      } else {
+        document.getElementById("myButton3").textContent = "No data found";
+      }
+    });
+  })
   
 });
 
--- scripts/content.js
+++ scripts/content.js
@@ -1,3 +1,5 @@
+//Calculate Current Grade Functionality
+
 const grid = document.getElementsByClassName("d2l-table d2l-grid d_gl");
 const myTable = grid[0];
 const numRows = myTable.rows.length;
@@ -33,5 +35,38 @@ for (i = 1; i < numRows; i++){
 
 sumWeight = sumWeight.toFixed(2);
 
-chrome.storage.local.set({ number: sumWeight }, () => {
+var gpa; 
+if (sumWeight <50){
+    gpa = "F";
+}else if (sumWeight >= 50 && sumWeight < 53 ){
+    gpa = "D-";
+}else if (sumWeight >= 53 && sumWeight < 57){
+    gpa ="D";
+}else if (sumWeight >= 57 && sumWeight < 60){
+    gpa = "D+";
+}else if (sumWeight >= 60 && sumWeight < 63){
+    gpa = "C-";
+}else if (sumWeight >= 63 && sumWeight < 67){
+    gpa = "C";
+}else if (sumWeight >= 67 && sumWeight < 70){
+    gpa ="C+";
+}else if (sumWeight >= 70 && sumWeight < 73){
+    gpa = "B-";
+}else if (sumWeight >= 73 && sumWeight < 77){
+    gpa = "B";
+}else if (sumWeight >= 77 && sumWeight < 80){
+    gpa = "B+";
+}else if (sumWeight >= 80 && sumWeight < 85){
+    gpa = "A-";
+}else if (sumWeight >= 85 && sumWeight < 90){
+    gpa = "A";
+}else{
+    gpa="A+";
+}
+
+console.log(sumWeight, gpa);
+chrome.storage.local.set({ number1: sumWeight }, () => {
+});
+
+chrome.storage.local.set({ number3: gpa }, () => {
 });
